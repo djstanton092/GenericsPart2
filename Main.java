ba27cbd66d0cace780da63f94bddab666faa58e4
Dan Stanton <djstanton@gmail.com>
2021-07-27 14:39:18 -0700

    Changes to beginning of part 3 generics

--- Main.java
+++ Main.java
@@ -12,5 +12,26 @@ public class Main {
         // adelaideCrows.addPlayer(beckham);
 
         System.out.println(adelaideCrows.numPlayers());
+
+        Team<BaseballPlayer> baseballTeam = new Team<>("Chicago Cubs");
+        baseballTeam.addPlayer(pat);
+        
+        Team<SoccerPlayer> brokenTeam = new Team<>("This won't work");
+        brokenTeam.addPlayer(beckham);
+
+        Team<FootballPlayer> melbourne = new Team<>("Melbourne");
+        FootballPlayer banks = new FootballPlayer("Gordon");
+        melbourne.addPlayer(banks);
+
+        Team<FootballPlayer> hawthorn = new Team<>("Hawthorn");
+        Team<FootballPlayer> fremantle = new Team<>("Fremantle");
+
+        hawthorn.matchResult(fremantle,1,0);
+        hawthorn.matchResult(adelaideCrows,3,8);
+       
+        adelaideCrows.matchResult(fremantle, 2, 1);
+//        adelaideCrows.matchResult(baseballTeam, 1,1);
+        
+
     }
 }
--- Team.java
+++ Team.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  *
  */
-public class Team<T> {
+public class Team<T extends Player> {
     private String name;
     int played = 0;
     int won = 0;
@@ -24,11 +24,11 @@ public class Team<T> {
 
     public boolean addPlayer(T player) {
         if (members.contains(player)) {
-            System.out.println(((Player)player).getName() + " is already on this team");
+            System.out.println(player.getName() + " is already on this team");
             return false;
         } else {
             members.add(player);
-            System.out.println(((Player)player).getName() + " picked for team " + this.name);
+            System.out.println(player.getName() + " picked for team " + this.name);
             return true;
         }
     }
@@ -37,17 +37,25 @@ public class Team<T> {
         return this.members.size();
     }
 
-    public void matchResult(Team opponent, int ourScore, int theirScore) {
+    public void matchResult(Team<T> opponent, int ourScore, int theirScore) {
+
+        String message;
+
         if (ourScore > theirScore) {
             won++;
+            message = " beat ";
         } else if (ourScore == theirScore) {
             tied++;
+            message = " drew with ";
+
         } else {
             lost++;
+            message = " lost to ";
         }
 
         played++;
         if (opponent != null) {
+            System.out.println(this.getName() + message + opponent.getName());
             opponent.matchResult(null, theirScore, ourScore);
         }
     }
